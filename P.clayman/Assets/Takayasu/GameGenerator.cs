e313475b8bf1e9b2b99aec2b8f3b685babc32063
DESKTOP-SIINF0D\takay <hideto-t-17e@stu.kbc.ac.jp>
2018-06-21 19:31:32 +0900

    [更新]蒲田君を更新しました。

---
+++ P.clayman/Assets/Takayasu/GameGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGenerator : MonoBehaviour {
+
+    public GameObject Enemy;
+    public float span = 1.0f;
+    float delta = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span) {
+            this.delta = 0;
+            GameObject enemy = Instantiate(Enemy) as GameObject;
+            float x = Random.Range(-300, 300);
+            float z = Random.Range(0, 200);
+            enemy.transform.position = new Vector3(x, 20, z);
+        }
+	}
+}
