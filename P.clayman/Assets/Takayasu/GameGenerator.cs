06735e2623147a2dacfddc053153f0394d1b8cbb
LAPTOP-LSSKFDV4\nkyyut <nktyut88@gmail.com>
2018-06-26 17:37:07 +0900

    [修正]ファイルを修正しました

---
+++ P.clayman/Assets/Takayasu/GameGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGenerator : MonoBehaviour {
+
+    public GameObject kabata;
+    public GameObject[] enemy;
+    private float timespan = 10.0f;
+    private float time = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > timespan)
+        {
+            GameObject go = Instantiate(kabata) as GameObject;
+            int pos = Random.Range(0, 3);
+            go.transform.position = enemy[pos].transform.position;
+            go.transform.rotation = enemy[pos].transform.rotation;
+            time = 0;
+            Debug.Log("Create Success");
+        }
+    }
+}
---
+++ P.clayman/Assets/Takayasu/Kabatakun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kabatakun : MonoBehaviour
+{
+    private float speed = 50.0f;
+    private Transform enemy;
+
+    private void Start()
+    {
+        // 城の位置取得
+        enemy = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        // 城の位置を向く
+        Quaternion targetRotation = Quaternion.LookRotation(enemy.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
+        // 前方に進む
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    // 衝突
+    void OnCollisionEnter(Collision col)
+    {
+        // 消える
+        Destroy(gameObject);
+    }
+}
