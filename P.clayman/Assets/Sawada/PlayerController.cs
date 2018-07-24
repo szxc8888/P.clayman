2f31240fda4218e0f4df2c11dcc2066d082d3e98
LAPTOP-B3IIV33C\youjiro <youjiro.yume.0209@gmail.com>
2018-07-24 13:02:07 +0900

    [更新]兵士の動きを更新しました。

--- P.clayman/Assets/Sawada/PlayerController.cs
+++ P.clayman/Assets/Sawada/PlayerController.cs
@@ -2,20 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField] private float Speed = 1.0f;
+     //速さ
+    [SerializeField] private float Speed = 10;
 
-    Rigidbody rb;
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update() {
+	void Update () {
 
-        rb.AddForce(Speed, 0,0);
+        if (Input.GetKey("right"))
+        {
+            this.transform.Translate(Speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            this.transform.Translate(-Speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            this.transform.Translate(0, 0, Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            this.transform.Translate(0, 0, -Speed * Time.deltaTime);
+        }
 	}
 }
